51da70e27c408ea9d3af33d8bf38f4d5d4a01a1e
Yahya Obeido <yahya.obeido@gmail.com>
2026-03-11 06:12:16 +0300

    Test change: increment 10 instead of 5

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 5)}>
+        <button onClick={() => setCount((count) => count + 10)}>
           count is {count}
         </button>
         <p>
